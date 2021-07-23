ec4604cbd3b8c7890350cf3f375d5822be368b87
Ezequiel Ramirez <ezequielram@gmail.com>
2021-07-23 17:14:10 -0300

    icono nav e itemlist container

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,7 +1,8 @@
 const ItemListContainer = ({greeting}) =>
-
-    <h1>Hola!! {greeting}</h1>
-
+<>
+    <h1 style={{color:'red'}}>Hola!! {greeting}</h1>
+    <p>Esta es nuestra tienda donde encontrara diferentes productos para su casa</p>
+</>
 
 
 export default ItemListContainer
