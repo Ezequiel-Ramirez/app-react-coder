b79dd72ad0b22fddd6c1b57c9139a47ca19af118
Ezequiel Ramirez <ezequielram@gmail.com>
2021-07-27 07:16:44 -0300

    hooks

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import ItemListContainer from "./components/ItemListContainer"
 //bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const App = () => 
+const App = () => {
+    
+
+    return(
     <>
         <Header/>
         <ItemListContainer greeting="Bienvenidos a mi tienda On-Line"/>
     </>
+    )
+}
 export default App
