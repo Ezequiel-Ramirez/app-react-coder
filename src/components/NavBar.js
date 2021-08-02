edf7fda8d8afac8adb01ce556a9fe6aa461bb986
Ezequiel Ramirez <ezequielram@gmail.com>
2021-08-02 14:52:27 -0300

    comprimi imagenes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,7 +9,7 @@ const NavBar = () =>
     <>
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
   <Container>
-  <Navbar.Brand href="#">Shipping</Navbar.Brand>
+  <Navbar.Brand href="#">Center Bike</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="m-auto">
