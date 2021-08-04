284dda9112533d787faa555cf6ea6cbdcb3c44b0
Ezequiel Ramirez <ezequielram@gmail.com>
2021-08-04 15:31:21 -0300

    modal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header"
 import ItemListContainer from "./components/ItemListContainer"
 import ItemDetailContainer from "./components/ItemDetailContainer";
+import { BrowserRouter, Route } from "react-router-dom";
 //bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -9,9 +10,11 @@ const App = () => {
 
     return(
     <>
+    <BrowserRouter>
         <Header/>
         <ItemListContainer greeting="Bienvenidos a mi tienda On-Line"/>
         <ItemDetailContainer/>
+    </BrowserRouter>
     </>
     )
 }
