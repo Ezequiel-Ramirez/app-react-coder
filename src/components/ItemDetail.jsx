06083012bb28bca518a5c7de38306ab3107f716b
Ezequiel Ramirez <ezequielram@gmail.com>
2021-08-08 14:26:45 -0300

    ajuste de itemdetail

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
+import CardColumns from 'react-bootstrap/CardColumns'
+import Card from 'react-bootstrap/Card'
+
 
 const ItemDetail = ({item}) => {
     return (
-        <>
-        
-        <h3>{item.title}</h3>
-        <h3>{item.category} </h3>
-        <h5>{item.description}</h5>
-        <h5>{item.price}</h5>
-           
-        
+        <>        
+        <CardColumns>
+  <Card>
+    <Card.Body>
+      <Card.Title><h1>{item.title}</h1>
+      </Card.Title>
+      <Card.Subtitle>{item.category}</Card.Subtitle>
+    <Card.Img variant="top" src={item.image} style={{width:'20%', margin: 'auto', display:'block'}} />
+      <Card.Text>ID: {item.id}</Card.Text>
+      <Card.Text>
+      {item.description}
+      </Card.Text>
+      <h5>$ {item.price}</h5>  
+    </Card.Body>
+  </Card>
+</CardColumns>
         </>
     )
 }
--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,35 +1,30 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import ItemDetail from "./ItemDetail"
 
 const ItemDetailContainer = () => {
 
     const [producto, setProducto] = useState([])
-    useEffect(()=>{
-    const getItem = new Promise ((res, rej) =>{
-    setTimeout(()=>{
-        const pedido = fetch('https://fakestoreapi.com/products/1')
-        pedido.then((respuesta)=>{
-            return respuesta.json()
+    useEffect(() => {
+        const getItem = new Promise((res, rej) => {
+            setTimeout(() => {
+                const pedido = fetch('https://fakestoreapi.com/products/1')
+                pedido.then((respuesta) => {
+                    return respuesta.json()
+                })
+                    .then((transformacion) => {
+                        res(transformacion)
+                    })
+            }, 2000)
         })
-        .then((transformacion)=>{
-            res(transformacion)
+        getItem.then((data_product) => {
+            setProducto(data_product)
         })
-    },2000)
-})
-getItem.then((data_product)=>{
-    setProducto(data_product)
-})
-   
-        
-       
-    },[])
+    }, [])
     console.log(producto)
-    
-   
 
     return (
         <div>
-             <ItemDetail item={producto}/>
+            <ItemDetail item={producto} />
         </div>
     )
 }
